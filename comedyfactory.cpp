56f2a1c4e5dd47a9c198cc42113a3aeef1896b36
Axel Sundstrom <axelsu@hotmail.com>
2025-03-09 00:07:38 -0800

    rough idea for comedyfactory

--- comedyfactory.cpp
+++ comedyfactory.cpp
@@ -1,4 +1,5 @@
 #include "comedyfactory.h"
+#include "comedymovie.h"
 
 std::shared_ptr<Movie> createMovie(const MovieParams& params){
    return std::make_shared<ComedyMovie>(
@@ -7,5 +8,15 @@ std::shared_ptr<Movie> createMovie(const MovieParams& params){
        params.releaseYear, 
        params.stock, 
        params.media
+   );
+}
+
+std::shared_ptr<Movie> createSearchKey(const MovieParams& params){
+   return std::make_shared<ComedyMovie>(
+      params.title, 
+      "", 
+      params.releaseYear, 
+      0, 
+      nullptr
    );
 }
