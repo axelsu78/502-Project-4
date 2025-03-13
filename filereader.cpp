e484a897efe0bd25d80a886b02cb910232fbe303
Cameron Chen <cameronchen08@gmail.com>
2025-03-13 15:53:44 -0700

    file reader class

---
+++ filereader.cpp
@@ -0,0 +1,46 @@
+ /* Axel Sundstrom, Cameron Chen
+    CSS 502 Assignment 4
+    File Reader Class
+ */
+
+ #include "filereader.h"
+ #include <fstream>
+ #include <iostream>
+ using namespace std;
+
+ void FileReader::readCommands(ifstream infile) {
+    string command;
+    while (infile >> command) {
+        cout << command << endl;
+    }
+ }
+
+ void FileReader::readCustomers(ifstream infile) {
+    string id;
+    string firstName;
+    string lastName;
+    while (infile >> id >> firstName >> lastName) {
+        cout << id << " " << firstName << " " << lastName << endl;
+    }
+ }
+ void FileReader::readMovies(ifstream infile) {
+    string type;
+    string stock;
+    string director;
+    string title;
+    string actor;
+    string releaseMonth;
+    string releaseYear;
+    while (infile >> type) {
+        if (type == "F") {
+            infile >> stock >> director >> title >> releaseYear;
+            cout << type << " " << stock << " " << director << " " << title << " " << releaseYear << endl;
+        } else if (type == "D") {
+            infile >> stock >> director >> title >> releaseYear;
+            cout << type << " " << stock << " " << director << " " << title << " " << releaseYear << endl;
+        } else if (type == "C") {
+            infile >> stock >> director >> title >> actor >> releaseMonth >> releaseYear;
+            cout << type << " " << stock << " " << director << " " << title << " " << actor << " " << releaseMonth << " " << releaseYear << endl;
+        }
+    }   
+ }
