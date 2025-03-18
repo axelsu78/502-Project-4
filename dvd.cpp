a23b70f4d081e8ed28c07dc5a9be2380c770ee42
Cameron Chen <cameronchen08@gmail.com>
2025-03-18 11:31:22 -0700

    dvd.cpp

---
+++ dvd.cpp
@@ -0,0 +1,14 @@
+/* Axel Sundstrom, Cameron Chen
+   CSS 502 Assignment 4
+   DVD Class
+*/
+
+#include "dvd.h"
+#include <iostream>
+using namespace std;
+
+DVD::~DVD() = default;
+
+string DVD::getMediaType() const {
+    return "DVD";
+}
