d68170d6b97e82638f76334b74de25faae7476ed
Cameron Chen <cameronchen08@gmail.com>
2025-03-18 11:31:08 -0700

    customestorage.cpp

---
+++ customerstorage.cpp
@@ -0,0 +1,17 @@
+/* Axel Sundstrom, Cameron Chen
+   CSS 502 Assignment 4
+   Customer Storage Class
+*/
+
+#include "customerstorage.h"
+#include "customer.h"
+#include "hashtable.h"
+#include <iostream>
+using namespace std;
+
+void CustomerStorage::getHistory(int customerID) {
+    cout << "CustomerStorage::getHistory" << endl;
+};
+
+
+
